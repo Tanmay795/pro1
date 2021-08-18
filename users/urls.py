c2d2da67899af4d63005279e04d2cd89963454ba
Tanmay795 <88677001+Tanmay795@users.noreply.github.com>
2021-08-18 15:50:51 +0530

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from. import views
 from django.contrib.auth import views as auth_view
 
 urlpatterns = [
-    path('login/', auth_view.LoginView.as_view(template_name='users/home.html'), name="login"),
+    path('', auth_view.LoginView.as_view(template_name='users/home.html'), name="login"),
     path('register/',views.register, name='register'),
     path('profile/', views.profile, name='profile'),
 ]
